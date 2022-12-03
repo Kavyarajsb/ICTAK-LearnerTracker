fd4f463a3581381f68f926294c512919c695914f
remya-nirem <remya.nirem@gmail.com>
2022-12-03 17:07:18 +0530

    training head and placement officer UI experience updated

--- Frontend/src/app/components/member/member.component.ts
+++ Frontend/src/app/components/member/member.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit,  ViewChild, AfterViewInit } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { AddNewMemberComponent } from '../add-new-member/add-new-member.component';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
-
 
 
 @Component({
@@ -16,85 +13,85 @@ import { MatPaginator } from '@angular/material/paginator';
   templateUrl: './member.component.html',
   styleUrls: ['./member.component.css']
 })
-export class MemberComponent implements OnInit,AfterViewInit {
+export class MemberComponent implements OnInit, AfterViewInit {
 
-  @ViewChild(MatSort) sort:any = MatSort;
-  @ViewChild(MatPaginator) paginator:any = MatPaginator;
+  @ViewChild(MatSort) sort: any = MatSort;
+  @ViewChild(MatPaginator) paginator: any = MatPaginator;
   public members = new MatTableDataSource<MemberData>();
 
-  constructor(private router:Router,private api:ApiService,private dialog: MatDialog) { }
+  constructor(private router: Router, private api: ApiService, private dialog: MatDialog) { }
 
-  id: any;  
-  displayedColumns: string[] = ['no','name','email', 'role','update', 'delete'];
-  
+  id: any;
+  displayedColumns: string[] = ['no', 'name', 'email', 'role', 'update', 'delete'];
+
   ngOnInit(): void {
-   this.getData();
-      }
+    this.getData();
+  }
 
-      applyFilter(filterValue: string) {
-        filterValue = filterValue.trim(); 
-        filterValue = filterValue.toLowerCase(); 
-        this.members.filter = filterValue;
-      }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.members.filter = filterValue;
+  }
 
-      ngAfterViewInit(): void {
-        this.members.sort = this.sort;
-        this.members.paginator = this.paginator;
-      }
+  ngAfterViewInit(): void {
+    this.members.sort = this.sort;
+    this.members.paginator = this.paginator;
+  }
 
-      getData(){
-        this.api.getMemberList().subscribe(res=>{
-        this.members.data = res as MemberData[];
-        })  
-      }
+  getData() {
+    this.api.getMemberList().subscribe(res => {
+      this.members.data = res as MemberData[];
+    })
+  }
 
-      updateData(id:any){
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.disableClose = true;
-        dialogConfig.autoFocus = true;
-        dialogConfig.width = "500px";
-  
-        dialogConfig.data = {
-          titlemode: 'Edit Staff Details',
-          id: id
-        };
+  updateData(id: any) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "500px";
 
-        const dialogRef = this.dialog.open(AddNewMemberComponent, dialogConfig);
-        dialogRef.afterClosed().subscribe(
-        data => {
-          this.api.updateMemberDetails(data).subscribe(res =>{
-            console.log('Member updated successfully');
-            this.getData();
-          })
-        }
-      )
-      }    
-    
-      addnew(){
-      const dialogConfig = new MatDialogConfig();
-      dialogConfig.disableClose = true;
-      dialogConfig.autoFocus = true;
-      dialogConfig.width = "500px";
+    dialogConfig.data = {
+      titlemode: 'Edit Staff Details',
+      id: id
+    };
 
-      dialogConfig.data = {
-      titlemode: 'Add Staff Details',
-      id:''
-      };
-  
-      const dialogRef = this.dialog.open(AddNewMemberComponent, dialogConfig);
-    
-      dialogRef.afterClosed().subscribe(
+    const dialogRef = this.dialog.open(AddNewMemberComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe(
       data => {
-        this.api.addNewMember(data).subscribe(res =>{
+        this.api.updateMemberDetails(data).subscribe(res => {
+          console.log('Member updated successfully');
+          this.getData();
+        })
+      }
+    )
+  }
+
+  addnew() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "500px";
+
+    dialogConfig.data = {
+      titlemode: 'Add Staff Details',
+      id: ''
+    };
+
+    const dialogRef = this.dialog.open(AddNewMemberComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(
+      data => {
+        this.api.addNewMember(data).subscribe(res => {
           console.log('Member added successfully');
           this.getData();
         })
       }
-    ); 
+    );
   }
 
-  deleteData(id:any){
-    this.api.deleteMemberDetails(id).subscribe(res =>{
+  deleteData(id: any) {
+    this.api.deleteMemberDetails(id).subscribe(res => {
       this.getData();
     });
   }
@@ -106,7 +103,7 @@ export interface MemberData {
   name: String;
   email: String;
   password: String;
-  role: String;  
+  role: String;
 }
 
 
